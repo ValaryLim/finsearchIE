00f65b959b7e1a8ab529ab0778e0b2ff3ced283d
Valary Lim Wan Qian <valarylim@outlook.com>
2022-02-03 05:59:55 +0800

    Add navbar to Finsearch app

--- finsearch_frontend/src/App.vue
+++ finsearch_frontend/src/App.vue
@@ -1,27 +1,63 @@
 <template>
-    <div id="app">
-        <router-view/>
-    </div>
+    <v-app class="overflow-hidden">
+        <v-app-bar
+            app
+            color="primary" dark
+            prominent dense
+            scroll-target="#scrolling-techniques-3"
+        >
+        <v-app-bar-title class="text-no-wrap">
+            <router-link to="/" tag="span" style="cursor: pointer">
+                {{ title }}
+            </router-link>
+        </v-app-bar-title>
+        <template v-slot:extension>
+            <v-tabs align-with-title>
+                <v-tab v-for="item in menu_items" :key="item.title" :to="item.path">
+                    {{ item.title }}
+                </v-tab>
+            </v-tabs>
+        </template>
+        </v-app-bar>
+        <v-sheet
+            id="scrolling-techniques-3"
+            class="overflow-y-auto"
+        >
+            <v-container style="height: 20px;"></v-container>
+        </v-sheet>
+
+        <v-content>
+            <router-view></router-view>
+        </v-content>
+    </v-app>
 </template>
 
 <script>
 export default {
     name: 'App',
+    data() {
+        return {
+            title: 'FinSearch', 
+            sidebar: false,
+            menu_items: [
+                { title: 'About', path:'/' },
+                { title: 'Search', path:'/search' },
+            ]
+        }
+    }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin: 30px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
 }
 
-.v-data-table-header th { 
-  /* used to align table */
-  white-space: nowrap;
+.v-app-bar-title__content {
+    width: 200px !important;
+    margin-left: -22px;
 }
 </style>
