ad470dab1dd138fe4e9db5c9e74b4f507292c8bf
Valary Lim Wan Qian <valarylim@outlook.com>
2022-02-08 03:59:09 +0800

    update finsearch to accept multiple models

--- utils/dir_processing.py
+++ utils/dir_processing.py
@@ -1,5 +1,11 @@
 import os
+import json
 
 def make_dir(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
+
+def save_json(file_path, abstract_list):
+        with open(file_path, "w") as f:
+            for abstract in abstract_list:
+                print(json.dumps(abstract), file=f)
